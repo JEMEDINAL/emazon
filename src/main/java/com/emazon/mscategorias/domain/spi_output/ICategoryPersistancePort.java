aebfc83a6056b7a85d9efa0798073c9802ec156c
JEMEDINAL <medinalondon2004@gmail.com>
2024-09-02 20:41:13 -0500

    Paginado personalizado realizado

--- src/main/java/com/emazon/mscategorias/domain/spi_output/ICategoryPersistancePort.java
+++ src/main/java/com/emazon/mscategorias/domain/spi_output/ICategoryPersistancePort.java
@@ -1,19 +1,16 @@
 package com.emazon.mscategorias.domain.spi_output;
 
 import com.emazon.mscategorias.domain.model.Category;
+import com.emazon.mscategorias.domain.model.CustomPageResponse;
 
-import java.util.List;
+
 
 public interface ICategoryPersistancePort {
     void saveCategory(Category category);
 
-    List<Category> getAllCategories();
 
-    List<Category> getParameterizedCategories(int page, int size, String orden);
 
-    Category getCategory(Long idCategory);
+    CustomPageResponse<Category> getParameterizedCategories(int page, int size, String orden);
 
-    void updateCategory(Category category);
 
-    void deleteCategory(Long idCategory);
 }
--- src/main/java/com/emazon/mscategorias/domain/usecase/CategoryUseCase.java
+++ src/main/java/com/emazon/mscategorias/domain/usecase/CategoryUseCase.java
@@ -3,6 +3,7 @@ package com.emazon.mscategorias.domain.usecase;
 import com.emazon.mscategorias.domain.api_input.ICategoryServicePort;
 
 import com.emazon.mscategorias.domain.model.Category;
+import com.emazon.mscategorias.domain.model.CustomPageResponse;
 import com.emazon.mscategorias.domain.spi_output.ICategoryPersistancePort;
 
 import com.emazon.mscategorias.infrastructure.exception.NameCategoryNotBlankException;
@@ -10,7 +11,7 @@ import com.emazon.mscategorias.infrastructure.exception.UnauthorizedUserExceptio
 import com.emazon.mscategorias.infrastructure.exception.ValidCategoryDescription;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
+
 import java.util.List;
 
 @Service
@@ -24,19 +25,21 @@ public class CategoryUseCase implements ICategoryServicePort {
     @Override
     public void saveCategory(Category category) {
         String userRol = "ADMIN";
+
         validAdminUser(userRol);
-        validCategoryName(category.getName());
-        validCategoryDescription(category.getDescription());
+
+        if(category.getName() == null || category.getName().trim().isEmpty() || category.getName().length() >= 50){
+            throw new NameCategoryNotBlankException();
+        }
+
+        if(category.getDescription() == null ||  category.getDescription().trim().isEmpty() || category.getDescription().length() > 90){
+            throw new ValidCategoryDescription();
+        }
 
         categoryPersistancePort.saveCategory(category);
 
-
     }
 
-    @Override
-    public List<Category> getAllCategories() {
-        return List.of();
-    }
 
     private  void validAdminUser(String rol){
         if(!rol.equals("ADMIN")){
@@ -44,22 +47,9 @@ public class CategoryUseCase implements ICategoryServicePort {
         }
     }
 
-    private void validCategoryName(String name){
-        if(name == null || name.trim().isEmpty() || name.length() >= 50){
-            throw new NameCategoryNotBlankException();
-        }
-    }
-
-    private void validCategoryDescription(String description){
-        if(description == null || description.trim().isEmpty() || description.length() > 90){
-            throw new ValidCategoryDescription();
-        }
-    }
-
-
 
     @Override
-    public List<Category> getParameterizedCategories(int page, int size,String orden) {
+    public CustomPageResponse<Category> getParameterizedCategories(int page, int size, String orden) {
         return categoryPersistancePort.getParameterizedCategories(page, size, orden);
     }
 
